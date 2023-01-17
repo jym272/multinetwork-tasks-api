7707a91db8a01bc4ba08a4f2cc01653ad457cc2e
Jorge Clavijo <jym272@gmail.com>
2023-01-17 17:30:46 -0300

    feat(test): home and utils routes tested

--- cmds.sh
+++ cmds.sh
@@ -7,3 +7,7 @@ token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoZW50aWNhdGUiOnRydWUsImlhdCI6M
 curl -s -X POST -H "Authorization: Bearer $token"  -H "Content-Type: application/json" -d '{"name": "new-task", "description": "some description"}' http://localhost:3052/new-task | jq
 # get all tasks
 curl -s -H "Authorization: Bearer $token" http://localhost:3052/get-all | jq
+# list all ports in use
+lsof -i -P -n | grep LISTEN | awk '{print $9}'
+# get a list of all ports in use
+lsof -i -P -n | grep LISTEN | awk '{print $9}' | cut -d ':' -f 2 | sort -n
